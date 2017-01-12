e79c96ff828c5d3ba179e86ae97afc256b4a2409
afghl <airport.0930@gmail.com>
2017-01-12 23:47:00 +0800

    add data rest starter to save much work

--- src/main/java/com/moments/controllers/MomentsController.java
+++ src/main/java/com/moments/controllers/MomentsController.java
@@ -7,6 +7,7 @@ import com.moments.services.moments.MomentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +22,7 @@ public class MomentsController {
     @Autowired
     private MomentService service;
 
+    @CrossOrigin
     @RequestMapping(value = "/users/{userId}/moments", method = GET)
     public String index(
             @PathVariable Integer userId,
