d361e83a30d2d622835f86188c63b73376947e24
afghl <airport.0930@gmail.com>
2017-01-18 14:13:51 +0800

    enable async

--- src/main/java/com/moments/MomentsApplication.java
+++ src/main/java/com/moments/MomentsApplication.java
@@ -2,8 +2,10 @@ package com.moments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class MomentsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MomentsApplication.class, args);
--- src/main/java/com/moments/controllers/FeedsController.java
+++ src/main/java/com/moments/controllers/FeedsController.java
@@ -55,7 +55,7 @@ public class FeedsController {
             jobs.syncUserFeedToRedis(currentUser);
 
         model.addAttribute("items", feed);
-
         return "jsonTemplate";
     }
+
 }
